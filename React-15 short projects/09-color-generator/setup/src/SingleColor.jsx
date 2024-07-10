803fd307bc0cd9a6beed4af2b5176b93d55c3699
Vedant Sonawane <98942578+Vedant2s@users.noreply.github.com>
2024-07-10 16:56:29 +0530

    Add files via upload

---
+++ React-15 short projects/09-color-generator/setup/src/SingleColor.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
+
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+  // console.log(hexColor);
+  useEffect(() => {
+    let timing = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timing);
+  }, [alert]);
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">Copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
